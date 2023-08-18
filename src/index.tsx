6d1136a7b62908ef31b78a4831dc121d11bd09f0
ataideverton <everton.ataide@vtex.com.br>
2023-08-18 13:28:42 -0300

    Fix key

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { FlagProvider } from '@unleash/proxy-client-react';
 
 const config = {
   url: 'https://us.app.unleash-hosted.com/ushh0018/api/frontend', // Your front-end API URL or the Unleash proxy's URL (https://<proxy-url>/proxy)
-  clientKey: 'sales-app:production.3428c27d7746e1aad9f0c1dd597ef53c5fdde03986f8253d6a63c20f', // A client-side API token OR one of your proxy's designated client keys (previously known as proxy secrets)
+  clientKey: '{{clientSideKey}}', // A client-side API token OR one of your proxy's designated client keys (previously known as proxy secrets)
   refreshInterval: 120, // How often (in seconds) the client should poll the proxy for updates
   appName: 'sales-app', // The name of your application. It's only used for identifying your application
   environment: 'production',
